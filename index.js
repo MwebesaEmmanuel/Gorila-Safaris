07f0f84b7eaa18552e80c1a4b12e87441d0e2caa
MwebesaEmmanuel <49569630+MwebesaEmmanuel@users.noreply.github.com>
2020-08-18 11:46:46 +0300

    First Change

---
+++ index.js
@@ -0,0 +1,20 @@
+function facebook(){
+    window.open("https://www.facebook.com/Wilddiscoveriessafaris?_rdc=1&_rdr")
+    }
+
+    function twiter(){
+        window.open("https://twitter.com/WDiscoveriess")
+    
+        }
+
+        function instagram(){
+            window.open("https://www.instagram.com/wilddiscoveries/")
+        
+            }
+    
+            function instagram(){
+                window.open("https://www.instagram.com/wilddiscoveries/")
+            
+                }
+        
+
